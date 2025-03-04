982719462a675b4c74cf3f691454b7441799f42e
sofia-speed <118084108+sofia-speed@users.noreply.github.com>
2025-03-04 12:32:42 +0000

    Update ALU_8bits

--- Projeto_1_AC_Processador-FPGA.srcs/sources_1/new/ALU_8bits.vhd
+++ Projeto_1_AC_Processador-FPGA.srcs/sources_1/new/ALU_8bits.vhd
@@ -1,42 +1,59 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity ALU_8bits is
-    Port ( a : in  STD_LOGIC_VECTOR (7 downto 0);
-           b : in  STD_LOGIC_VECTOR (7 downto 0);
-           op : in  STD_LOGIC_VECTOR (3 downto 0);
-           cin : in  STD_LOGIC;  
-           res : out STD_LOGIC_VECTOR (7 downto 0);
-           cout : out STD_LOGIC);
+    Port (
+        A        : in  STD_LOGIC_VECTOR(7 downto 0);  
+        B        : in  STD_LOGIC_VECTOR(7 downto 0);  
+        Sel      : in  STD_LOGIC_VECTOR(3 downto 0);  
+        Result   : out STD_LOGIC_VECTOR(7 downto 0);  
+        COMP_FLAG: out STD_LOGIC_VECTOR(4 downto 0)   
+    );
 end ALU_8bits;
 
-architecture Structural of ALU_8bits is
-
-    component ALU 
-        Port ( a : in  STD_LOGIC;
-               b : in  STD_LOGIC;
-               op : in  STD_LOGIC_VECTOR (3 downto 0);
-               cin : in  STD_LOGIC;
-               res : out STD_LOGIC;
-               cout : out STD_LOGIC);
-    end component;
-
-    signal carry: STD_LOGIC_VECTOR (8 downto 0); 
-
+architecture Behavioral of ALU_8bits is
+    signal Carry : STD_LOGIC_VECTOR(8 downto 0); 
+    signal Temp_Result : STD_LOGIC_VECTOR(7 downto 0); 
 begin
+    Carry(0) <= Sel(0); -- Se Sel(0) = 1, faz sub com carry = 1
 
-    carry(0) <= cin; 
+    
+    ALU_Bit0: entity work.ALU port map (A(0), B(0), Carry(0), Sel, Temp_Result(0), Carry(1));
+    ALU_Bit1: entity work.ALU port map (A(1), B(1), Carry(1), Sel, Temp_Result(1), Carry(2));
+    ALU_Bit2: entity work.ALU port map (A(2), B(2), Carry(2), Sel, Temp_Result(2), Carry(3));
+    ALU_Bit3: entity work.ALU port map (A(3), B(3), Carry(3), Sel, Temp_Result(3), Carry(4));
+    ALU_Bit4: entity work.ALU port map (A(4), B(4), Carry(4), Sel, Temp_Result(4), Carry(5));
+    ALU_Bit5: entity work.ALU port map (A(5), B(5), Carry(5), Sel, Temp_Result(5), Carry(6));
+    ALU_Bit6: entity work.ALU port map (A(6), B(6), Carry(6), Sel, Temp_Result(6), Carry(7));
+    ALU_Bit7: entity work.ALU port map (A(7), B(7), Carry(7), Sel, Temp_Result(7), Carry(8));
 
+    process(Temp_Result, Carry)
+    begin
+        -- A < B 
+        COMP_FLAG(0) <= Temp_Result(7); 
+        COMP_FLAG(4) <= NOT Temp_Result(7); 
 
-    ALU0: ALU port map(a => a(0), b => b(0), op => op, cin => carry(0), res => res(0), cout => carry(1));
-    ALU1: ALU port map(a => a(1), b => b(1), op => op, cin => carry(1), res => res(1), cout => carry(2));
-    ALU2: ALU port map(a => a(2), b => b(2), op => op, cin => carry(2), res => res(2), cout => carry(3));
-    ALU3: ALU port map(a => a(3), b => b(3), op => op, cin => carry(3), res => res(3), cout => carry(4));
-    ALU4: ALU port map(a => a(4), b => b(4), op => op, cin => carry(4), res => res(4), cout => carry(5));
-    ALU5: ALU port map(a => a(5), b => b(5), op => op, cin => carry(5), res => res(5), cout => carry(6));
-    ALU6: ALU port map(a => a(6), b => b(6), op => op, cin => carry(6), res => res(6), cout => carry(7));
-    ALU7: ALU port map(a => a(7), b => b(7), op => op, cin => carry(7), res => res(7), cout => carry(8));
+        if (Temp_Result = "00000000") then
+            COMP_FLAG(1) <= '1'; -- A <= B
+            COMP_FLAG(2) <= '1'; -- A = B
+            COMP_FLAG(3) <= '1'; -- A >= B
+            COMP_FLAG(4) <= '0'; -- A > B
+        else
+            COMP_FLAG(1) <= Temp_Result(7); -- A <= B ou A < B
+            COMP_FLAG(2) <= '0'; -- A = B 
+            COMP_FLAG(3) <= NOT (Temp_Result(7)); -- A >= B ou A >= B
+        end if;
+        
+    end process;
 
-    cout <= carry(8);
-
-end Structural;
+    process (Sel, Temp_Result)
+    begin
+        if Sel(3) = '1' then
+            Result <= (others => '0'); 
+        else 
+            Result <= Temp_Result;
+        end if;
+    end process;
+end Behavioral;
