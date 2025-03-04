37557ffb3f20a319cc7c12c7ceb109a837473078
sofia-speed <118084108+sofia-speed@users.noreply.github.com>
2025-03-04 16:29:56 +0000

    .

--- Projeto_1_AC_Processador-FPGA.srcs/sources_1/new/ALU.vhd
+++ Projeto_1_AC_Processador-FPGA.srcs/sources_1/new/ALU.vhd
@@ -3,12 +3,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ALU is
     Port (
-        A       : in  STD_LOGIC;  
-        B       : in  STD_LOGIC;
-        Cin     : in  STD_LOGIC;
-        Sel     : in  STD_LOGIC_VECTOR(3 downto 0); 
-        Result  : out STD_LOGIC;
-        Cout    : out STD_LOGIC 
+        A : in  STD_LOGIC;  
+        B : in  STD_LOGIC;
+        Cin: in  STD_LOGIC;
+        Sel : in  STD_LOGIC_VECTOR(3 downto 0); 
+        Result : out STD_LOGIC;
+        Cout : out STD_LOGIC 
     );
 end ALU;
 
@@ -18,8 +18,8 @@ begin
 
     Adder: entity work.Full_Adder
         port map (
-            A   => A,
-            B   => B,
+            A => A,
+            B => B,
             Cin => Cin,
             Sub => Sel(0),  -- Sel(0) = 1, ativa a subtração
             Sum => Sum,
