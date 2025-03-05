af8f0d94578e45451572f717f71cbc2c72edfe2e
sofia-speed <118084108+sofia-speed@users.noreply.github.com>
2025-03-05 16:44:45 +0000

    memoria de dados

---
+++ Projeto_1_AC_Processador-FPGA.srcs/sources_1/new/Memoria_dados.vhd
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity Memoria_dados is
+    Port (
+        CLK : in STD_LOGIC;
+        WR : in STD_LOGIC;
+        constante : in STD_LOGIC_VECTOR(7 downto 0);
+        Operando1 : in STD_LOGIC_VECTOR(7 downto 0);
+        Dados_M  : out STD_LOGIC_VECTOR(7 downto 0)
+    );
+end Memoria_dados;
+
+architecture Behavioral of Memoria_dados is
+    type mem_array is array (0 to 255) of STD_LOGIC_VECTOR(7 downto 0);
+    signal memoria : mem_array := (others => (others => '0'));
+begin
+    process(CLK)
+    begin
+        if rising_edge(CLK) then
+            if WR = '1' then
+                memoria(conv_integer(constante)) <= Operando1;
+            end if;
+            Dados_M <= memoria(conv_integer(constante));
+        end if;
+    end process;
+end Behavioral;
