ed8660da936f9e18b3a5b42dbfec93131578a38e
Jorro <carapau2002@gmail.com>
2025-03-11 21:11:42 +0000

    MotherBoard connectect to CPU ( not working )

--- Projeto_1_AC_Processador-FPGA.srcs/sim_1/new/MB_TestBench.vhd
+++ Projeto_1_AC_Processador-FPGA.srcs/sim_1/new/MB_TestBench.vhd
@@ -9,10 +9,10 @@ architecture testbench of MB_TestBench is
 	component MotherBoard
 	Port (
 		clk, WR : in std_logic;
-		opcode : out std_logic_vector(4 downto 0);
+		opcode_out : out std_logic_vector(4 downto 0);
 		SEL_REG1, SEL_REG2 : out std_logic_vector(2 downto 0); --
 		Dados_M : out std_logic_vector(7 downto 0);
-		Operando1 : in std_logic_vector(7 downto 0)
+		Operando1, Endereco: in std_logic_vector(7 downto 0)
 	);
 	end component;
 
@@ -31,10 +31,13 @@ architecture testbench of MB_TestBench is
         WR : out std_logic --
     );
 	end component;
-	signal PIN_tb  : std_logic_vector(7 downto 0) := (others => '0');
-	signal POUT_tb : std_logic_vector(7 downto 0);
-	signal clk_tb  : std_logic := '0';
-	signal reset_tb : std_logic := '0';
+
+	signal PIN_tb, POUT_tb : std_logic_vector(7 downto 0) := (others => '0');
+	signal Dados_M, Constante, Operando1, Endereco : std_logic_vector(7 downto 0);
+	signal clk_tb, reset_tb  : std_logic := '0';
+	signal WR: std_logic;
+	signal SEL_REG1, SEL_REG2 : std_logic_vector(2 downto 0);
+	signal opcode : std_logic_vector(4 downto 0);
 
 	-- Clock period
 	constant clk_period : time := 10 ns;
@@ -44,12 +47,30 @@ begin
 	-- Instantiate the CPU
 	UUT: MotherBoard
 		port map (
-			PIN   => PIN_tb,
-			reset => reset_tb,
-			clk   => clk_tb,
-			POUT  => POUT_tb
+			clk => clk_tb,
+			WR => WR,
+			opcode_out => opcode,
+			SEL_REG1 => SEL_REG1,
+			SEL_REG2 => SEL_REG2,
+			Dados_M => Dados_M,
+			Operando1 => Operando1,
+			Endereco => Endereco
 		);
-
+	UUT_CPU : CPU
+		port map (
+			PIN => PIN_tb,
+			reset => reset_tb,
+			clk => clk_tb,
+			POUT => Pout_tb,
+			Dados_M => Dados_M,
+			Constante => Constante,
+			SEL_REG1 => SEL_REG1,
+			SEL_REG2 => SEL_REG2,
+			Opcode => opcode,
+			Operando1_out => Operando1,
+			Endereco => Endereco,
+			WR => WR
+		);	
 	-- Clock process
 	process
 	begin
@@ -75,7 +96,6 @@ begin
 
         
 		-- End simulation
-		report "Test completed successfully!";
 		wait;
 	end process;
 
